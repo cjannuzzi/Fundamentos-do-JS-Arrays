f446dff3a461622a8cb59fe1b9971be1bd7e3c6e
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-03-01 21:35:42 -0300

    Criar a concatenação de duas salas de alunos que estudam JS e Python para uma única sala de aula

---
+++ juntando-turmas.js
@@ -0,0 +1,6 @@
+//vamos continuar a trabalhar com nossas listas de alunos e fazendo alterações, os alunos vão, os alunos vêm, vamos ver qual o desafio desta aula, que é juntar duas salas. Vai ter uma palestra sobre padrões de projeto para os alunos da sala de Java script e de Python. Vamos juntar ambas as salas em uma única lista para ir conferir a presença dessas duas turmas.
+
+const salaDePython = ['Melissa', 'Elena', 'Rodrigo'];
+const salaDeJavaScript = ['Ju', 'Leo', 'Raquel'];
+
+const salasUnificadas = salaDePython.concat(salaDeJavaScript)
