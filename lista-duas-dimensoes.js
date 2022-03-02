55bbf1c0ed7e8e508b25482daa2acf442b690065
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-03-01 21:42:54 -0300

    Criar lista com alguns alunos e alunas e depois criar outra lista com as médias seguidas dos nomes

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,3 @@
+//continuando então a manipulas as nossas listas de alunos e alunas, vamos ver qual o desafio que nos aguarda. Desta vez o desafio é criar uma lista com alguns alunos e alunas. João, Juliana, Caio e Ana, e depois criar uma outra lista com as médias, as seguintes médias, e temos que criar uma lista com duas dimensões, os nomes e as médias.
+
+const alunos = ['João', 'Juliana', 'Caio','Ana']
