07e7b54863bc56446f0129bd79ccc36393c7b1d1
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-03-01 21:39:27 -0300

    Adicionando o console no arquivo de concatenação

--- juntando-turmas.js
+++ juntando-turmas.js
@@ -3,4 +3,5 @@
 const salaDePython = ['Melissa', 'Elena', 'Rodrigo'];
 const salaDeJavaScript = ['Ju', 'Leo', 'Raquel'];
 
-const salasUnificadas = salaDePython.concat(salaDeJavaScript)
+const salasUnificadas = salaDePython.concat(salaDeJavaScript);
+console.log(salasUnificadas);
