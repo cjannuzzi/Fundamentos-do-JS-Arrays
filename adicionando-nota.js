03430457a7c40790327726de2a9ba99886c19b1b
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-03-01 21:00:04 -0300

    Modificar arquivo

--- adicionando-nota.js
+++ adicionando-nota.js
@@ -1,3 +1,7 @@
+// Desafio:
+// Um professor que deveria ter lançado quatro notas, de quatro bimestres de um aluno, por engano lançou só três. Então como fazemos para lançar a quarta nota em uma lista de notas? Abaixo a resolução.
+
+
 const notas = [10, 6, 8];
 notas.push(7);
 console.log(notas);
