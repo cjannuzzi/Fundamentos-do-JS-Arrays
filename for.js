ddda41dab3f327d6b910882910e8beae2e4354ca
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-03-02 11:54:22 -0300

    Criar uma rotina FOR que retorna o indice e seu respctivo valor em forma de lista

---
+++ for.js
@@ -0,0 +1,7 @@
+//Imprimir um índice e uma lista com os seguintes números. Agora vamos trabalhar com uma estrutura chamada for.
+
+const numeros = [100, 200, 300, 400, 500, 600];
+
+for (let i = 0; i< numeros.length; i++){
+    console.log(i, numeros[i])
+};
