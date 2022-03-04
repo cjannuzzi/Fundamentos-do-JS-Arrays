c2591241e4e538a8b821c6cead1808f50d82dd41
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-03-03 23:29:29 -0300

    Organizar adicionando ;

--- dividindo-arrays.js
+++ dividindo-arrays.js
@@ -2,8 +2,8 @@
 
 const nomes = [ 'João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjore', 'Guilherme', 'Aline', 'Fabiana', 'André', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela','Vinicius', 'Renan', 'Renata', 'Daisy', 'Camilo'];
 
-console.log( "Tamanho da array", nomes.length)
+console.log( "Tamanho da array", nomes.length);
 const sala1 = nomes.slice(0,nomes.length/2);
 const sala2 = nomes.slice(nomes.length/2);
-console.log (`Alunos da sala 1 ${sala1}`)
-console.log (`Alunos da sala 2 ${sala2}`)
+console.log (`Alunos da sala 1 ${sala1}`);
+console.log (`Alunos da sala 2 ${sala2}`);
